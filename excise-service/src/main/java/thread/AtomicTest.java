509229cb88943133fac904cdda711b3646cff85d
maguoqiang <maguoqiang@111.com.cn>
2021-06-03 14:23:51 +0800

    update .gitignore

--- excise-service/src/main/java/thread/AtomicTest.java
+++ excise-service/src/main/java/thread/AtomicTest.java
@@ -1,8 +1,74 @@
 package thread;
 
+import org.junit.Test;
+import sun.misc.Unsafe;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
 /**
  * @author maguoqiang
  * @date 2021-06-03 9:34
  */
 public class AtomicTest {
+
+    @Test
+    public void test01() throws InterruptedException {
+        AtomicInteger integer = new AtomicInteger(0);
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    integer.incrementAndGet();
+                }
+            }).start();
+        }
+        Thread.sleep(1000);
+        System.out.println(integer.get());
+    }
+
+
+    @Test
+    public void testUnsafe() throws NoSuchFieldException {
+        /*Unsafe unsafe = Unsafe.getUnsafe();
+        long ageOffset = unsafe.objectFieldOffset(UnsafeTest.class.getDeclaredField("age"));*/
+        UnsafeTest test = new UnsafeTest();
+        System.out.println(test);
+        //System.out.println("ageOffSet:========>" + ageOffset);
+    }
+
+    @Test
+    public void testAtomicArray(){
+        int[] value = new int[]{1,2};
+        //不修改原值。
+        AtomicIntegerArray aiArray = new AtomicIntegerArray(value);
+
+        aiArray.getAndSet(0,2);
+        System.out.println(aiArray.get(0));
+        System.out.println(value[0]);
+    }
+
+
+    static class UnsafeTest {
+        private String name;
+        private int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
+    //要用cas修改某个属性的值，首先要知道属性在对象在内存空间的哪个位置，必须知道属性的偏移量
+    //偏移量：某个属性相对于其对象的内存地址的位置。例：UnSafeTest的地址是：thread.AtomicTest$UnsafeTest@1376c05c，age的偏移量是10。
 }
--- excise-service/src/main/java/thread/SemaphoreTest.java
+++ excise-service/src/main/java/thread/SemaphoreTest.java
@@ -1,8 +1,73 @@
 package thread;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author maguoqiang
  * @date 2021-06-02 12:21
  */
 public class SemaphoreTest {
+
+
+
+
+    public static void main(String[] args) {
+        Semaphore s = new Semaphore(2);
+        for (int i=0;i<10;i++){
+            new Thread(new ThreadDeamo(s)).start();
+
+        }
+
+
+    }
+
+
+    static class ThreadDeamo implements Runnable{
+        Semaphore s ;
+
+
+
+        ThreadDeamo(Semaphore s){
+            this.s=s;
+        }
+
+        @Override
+        public void run() {
+            //可中断
+            try {
+
+                s.acquire();
+
+                if (Thread.currentThread().isInterrupted()){
+                    System.out.println("中断了。");
+                }
+
+                System.out.println(Thread.currentThread().getName()+"获得=========");
+                Thread.sleep(1000);
+                s.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //降级--超时后，直接舍弃
+            /*try {
+                if (s.tryAcquire(500, TimeUnit.MILLISECONDS)){
+
+                    System.out.println(Thread.currentThread().getName()+"获得=========");
+                    Thread.sleep(1000);
+                    s.release();
+                }else {
+                    fallback();
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }*/
+
+        }
+
+        private void fallback() {
+            System.out.println(Thread.currentThread().getName()+"降级");
+        }
+    }
 }
