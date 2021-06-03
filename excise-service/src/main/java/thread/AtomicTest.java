9e937b4b7cf28ed793917b8e12f1c4599c42166c
maguoqiang <maguoqiang@111.com.cn>
2021-06-03 14:22:35 +0800

    da

---
+++ excise-service/src/main/java/thread/AtomicTest.java
@@ -0,0 +1,8 @@
+package thread;
+
+/**
+ * @author maguoqiang
+ * @date 2021-06-03 9:34
+ */
+public class AtomicTest {
+}
---
+++ excise-service/src/main/java/thread/SemaphoreTest.java
@@ -0,0 +1,8 @@
+package thread;
+
+/**
+ * @author maguoqiang
+ * @date 2021-06-02 12:21
+ */
+public class SemaphoreTest {
+}
