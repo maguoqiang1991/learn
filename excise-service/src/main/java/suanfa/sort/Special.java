3fe96288c388869f56aeb70a2600ed18e7f404bc
maguoqiang <maguoqiang@111.com.cn>
2021-07-15 19:35:31 +0800

    tt

---
+++ excise-service/src/main/java/suanfa/sort/Special.java
@@ -0,0 +1,12 @@
+package suanfa.sort;
+import com.alibaba.fastjson.JSON;
+
+public class Special{
+    public static void main(String[] args) {
+        String s="您好，核实上传的面单图片非本单快递面单图片， 提供的照片证据不足，按退换货要求无法受理，你可以上传符合要求的照片 或者联系您对应的BD核查是否可以线下处理 ，谢谢！";
+        s = JSON.toJSONString(s);
+        System.out.println("===============");
+
+        System.out.println(s);
+    }
+}
